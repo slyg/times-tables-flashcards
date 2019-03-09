6443643a79161487a3ea7114f3d360c0f0e088f8
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-03-09 20:28:52 +0000

    Add UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "purple",
+  "maroon",
+  "violet",
+  "turquoise",
+  "greyish",
+  "default"
+];
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+const getRandomNum = () => Math.trunc(Math.random() * 11);
+
+const Times = ({ a, b }) => (
+  <p>
+    {a.toString()} x {b.toString()}
+  </p>
+);
+
+const Result = ({ result }) => <p>{result}</p>;
+
+const SPACE_KEY = 32;
 
 class App extends Component {
+  state = {
+    a: 1,
+    b: 1,
+    result: 1,
+    showResult: false,
+    color: "default"
+  };
+
+  handleKeyDown(event) {
+    switch (event.keyCode) {
+      case SPACE_KEY:
+        if (this.state.showResult === false) {
+          this.setState({
+            showResult: true
+          });
+        } else {
+          const [a, b, color] = [getRandomNum(), getRandomNum(), randomColor()];
+          const result = a * b;
+          this.setState({
+            a,
+            b,
+            result,
+            color,
+            showResult: false
+          });
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+
   render() {
+    const bgClass = `App-body App-body--${this.state.color}`;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={bgClass}>
+          {this.state.showResult ? (
+            <Result {...this.state} />
+          ) : (
+            <Times {...this.state} />
+          )}
+        </div>
       </div>
     );
   }
