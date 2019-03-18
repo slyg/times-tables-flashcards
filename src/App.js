3386e820ae906c3cdd73e225e3912fd57364e148
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-03-18 23:47:45 +0000

    Isolate effects

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { initialState, update } from "./state";
-
-const SPACE_KEY = 32;
-const RIGHT_ARROW_KEY = 39;
-const DOWN_ARROW_KEY = 40;
+import { keyDownUpdate } from "./effects";
 
 const App = () => {
   const [state, setState] = useState(initialState);
-
   const updateState = () => setState(update(state));
-
-  const handleKeyDown = event => {
-    switch (event.keyCode) {
-      case SPACE_KEY:
-      case RIGHT_ARROW_KEY:
-      case DOWN_ARROW_KEY:
-        updateState();
-        break;
-      default:
-        break;
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  });
+  useEffect(keyDownUpdate(updateState));
 
   const { a, b, result, showResult, color } = state;
 
---
+++ src/effects.js
@@ -0,0 +1,25 @@
+const SPACE_KEY = 32;
+const RIGHT_ARROW_KEY = 39;
+const DOWN_ARROW_KEY = 40;
+
+export const keyDownUpdate = cb => {
+  const handleKeyDown = event => {
+    switch (event.keyCode) {
+      case SPACE_KEY:
+      case RIGHT_ARROW_KEY:
+      case DOWN_ARROW_KEY:
+        cb();
+        break;
+      default:
+        break;
+    }
+  };
+  const effect = () => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  };
+
+  return effect;
+};
